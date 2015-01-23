aa434a411c79a4cab9d498ab3923f8c8be3f3df4
zariganitosh <XXXX@example.com>
2015-01-23 09:42:32 +0900

    オプション解析機能を追加

--- pentomino.rb
+++ pentomino.rb
@@ -1,5 +1,46 @@
+#!/usr/bin/ruby
 # coding: utf-8
-BROW, BCOL = 10, 6
+
+require 'optparse'
+
+# オプション解析
+$options = {}
+OptionParser.new do |opt|
+  opt.banner = 'Usage: pentomino [options] [board size(3-8)]'
+  opt.on('-q', 'Hide progress putting a piece on board.(quiet mode)') {|v| $options[:quiet] = v}
+  opt.separator('')
+  opt.on('board size:',
+         '    [3] x 20',
+         '    [4] x 15',
+         '    [5] x 12',
+         '    [6] x 10 (Default)',
+         '    [7] x  9 - 3',
+         '    [8] x  8 - 4',
+         )
+  opt.separator('')
+  opt.on('example:',
+         '            6 x 10                        7 x 9 - 3                   8 x 8 - 4',
+         '11 12 13 14 15 16 17 18 19 1A    11 12 13 14 15 16 17 18 19    11 12 13 14 15 16 17 18',
+         '21 22 23 24 25 26 27 28 29 2A    21 22 23 24 25 26 27 28 29    21 22 23 24 25 26 27 28',
+         '31 32 33 34 35 36 37 38 39 3A    31 32 33 34 35 36 37 38 39    31 32 33 34 35 36 37 38',
+         '41 42 43 44 45 46 47 48 49 4A    41 42 43          47 48 49    41 42 43       46 47 48',
+         '51 52 53 54 55 56 57 58 59 5A    51 52 53 54 55 56 57 58 59    51 52 53       56 57 58',
+         '61 62 63 64 65 66 67 68 69 6A    61 62 63 64 65 66 67 68 69    61 62 63 64 65 66 67 68',
+         '                                 71 72 73 74 75 76 77 78 79    71 72 73 74 75 76 77 78',
+         '                                                               81 82 83 84 85 86 87 88',
+         )
+  begin
+    opt.parse!(ARGV)
+    BCOL = (ARGV[0] || 6).to_i
+    BROW = 64 / BCOL
+    raise "Invalid board size: #{BCOL}" if BCOL < 3 || 8 < BCOL
+  rescue => e
+    puts e
+    exit
+  end
+end
+
+
 
 # すべてのピース形状をPieceオブジェクトの配列に保存する
 class Piece
@@ -55,6 +96,8 @@ board = Array.new((BROW + 1) * (BCOL + 1), 0)
 board.each_with_index do |b, i|
   board[i] = 100 if ((i + 1) % (BCOL + 1)) == 0 || i >= ((BCOL + 1) * BROW)
 end
+board[30], board[31], board[39], board[40] = 13, 13, 13, 13 if BCOL == 8
+board[27], board[35], board[43]            = 13, 13, 13     if BCOL == 7
 
 
 
@@ -91,7 +134,7 @@ def try_piece(board, pp, lvl)
       # ピースを置く
       blocks.each {|b| board[x + b] = i + 1}
       piece.used = true
-      display_board(board, pp)
+      display_board(board, pp) if !$options[:quiet]
       # すべてのピースを置ききったらTrueを返す（recursiveコールの終了）
       if lvl == 11 then
         $counter += 1
